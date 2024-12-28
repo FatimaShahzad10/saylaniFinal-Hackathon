7b1b10f70cc807233079fcc52579b2e5d406502b
FatimaShahzad10 <fatimashahzad202@gmail.com>
2024-12-29 00:40:14 +0500

    Add files via upload

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+    <div className="bg-primary w-full flex p-2 flex-col items-center justify-center">
+      <div className="w-32">
+       
+      </div>
+      <p className="font-bold text-white">
+        © 2024 notepad.com All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
