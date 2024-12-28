2c67169a2dc0f02ebce9489e80e3e0af9654918d
FatimaShahzad10 <fatimashahzad202@gmail.com>
2024-12-29 00:16:05 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#99CA3C",
+        secondary: "#0030B3",
+      },
+    },
+  },
+  plugins: [],
+};
